c42a788079de52765c0bfe82eeb40aff85ea6048
Nakib00 <2022142@iub.edu.bd>
2024-03-12 22:05:11 +0600

    add website

---
+++ resources/views/website/layout/webpage.blade.php
@@ -0,0 +1,14 @@
+{{--  include head  --}}
+@include('website.include.head')
+
+{{--  Include navigation ber  --}}
+@include('website.include.napver')
+
+<main>
+
+@yield('content')
+
+</main>
+
+{{--  include footer  --}}
+@include('website.include.footer')
