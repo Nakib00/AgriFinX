b29b010291fa63c6bb5acee00fe0f5ee07f23135
Nakib00 <2022142@iub.edu.bd>
2024-03-16 15:45:49 +0600

    add auth

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<h1  {{ $attributes }}>Agrifinx</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,31 @@
 <?php
 
+use App\Http\Controllers\{ProfileController,appsfunctioncontroller};
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{appsfunctioncontroller};
 
-
+// index page route start
 Route::get('/', function () {
     return view('website.index');
 });
+// index page route end
 
+// website route start
 Route::get('/microloan',[appsfunctioncontroller::class, 'mindex'])->name('mindex');
 Route::get('/incurance',[appsfunctioncontroller::class, 'iindex'])->name('iindex');
 Route::get('/agroproject',[appsfunctioncontroller::class, 'agropindex'])->name('agropindex');
 Route::get('/subsides',[appsfunctioncontroller::class, 'sindex'])->name('sindex');
+//website route end
+
+// admin routes start
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+// admin routes end
