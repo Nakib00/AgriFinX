ee0e43059e9fce4d905b3a57fce12913b13ef28f
Hridis <hridika781@gmail.com>
2024-03-30 00:19:30 +0600

    a

--- resources/views/components/application-logo.blade.php
+++
@@ -1,1 +0,0 @@
-<h1  {{ $attributes }}>Agrifinx</h1>
--- resources/views/website/users/agri_officer/button.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('website.users.agri_officer.layout.agriofficerlayout')
-@section('agriofficer.dashboard')
-    <h1 class="mb-4">content here</h1>
-    {{-- Your main content here --}}
-@endsection
