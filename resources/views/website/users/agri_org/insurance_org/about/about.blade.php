f2a4c1a257a46bbae6198e11c0c5a25b57747019
hridis <hridika781@gmail.com>
2024-04-05 13:54:09 +0600

    InsuranceAboutCSS

--- resources/views/website/users/agri_org/insurance_org/about/about.blade.php
+++ resources/views/website/users/agri_org/insurance_org/about/about.blade.php
@@ -3,25 +3,32 @@
     <div class="container mt-4">
         <div class="row">
             <div class="col-md-9 pb-3">
-                <div class="aboutcontainer">
+                <div class="about-container">
                     {{-- Check if $about is not empty --}}
                     @if ($about)
-                        <h1>About Details</h1>
-                        <p>{{ $about->about }}</p>
+                        <div class="about-section">
+                            <h1 class="section-title">About Details</h1>
+                            <p class="section-content">{{ $about->about }}</p>
+                        </div>
 
-                        <h1>Loan Providing Types</h1>
-                        <p>{!! $about->type_of_service !!}</p>
+                        <div class="about-section">
+                            <h1 class="section-title">Insurance Providing Types</h1>
+                            <p class="section-content">{!! $about->type_of_service !!}</p>
+                        </div>
 
-                        <h1>Team</h1>
-                        <p>{{ $about->team }}</p>
+                        <div class="about-section">
+                            <h1 class="section-title">Team</h1>
+                            <p class="section-content">{{ $about->team }}</p>
+                        </div>
 
-                        <h1>Conditions</h1>
-                        <p>{!! $about->conditions !!}</p>
+                        <div class="about-section">
+                            <h1 class="section-title">Conditions</h1>
+                            <p class="section-content">{!! $about->conditions !!}</p>
+                        </div>
 
-                        <a href="{{ route('insurance.editabout') }}"><button type="submit">Make update</button></a>
+                        <a href="{{ route('insurance.editabout') }}" class="btn btn-success">Make Update</a>
                     @else
-                        <p>No about information available.</p>
-
+                        <p class="no-info-message">No about information available.</p>
                         {{-- Button to add new information --}}
                         <a href="{{ route('insurance.addAbout') }}" class="btn btn-primary">Add Information</a>
                     @endif
